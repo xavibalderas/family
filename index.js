d217b65bcc355bc3e7ee5aa3c6fc68f2c7330e62
Francisco Javier Balderas Cejudo <frbal5@retchso-xw0007.ikea.com>
2018-12-13 08:47:42 +0100

    Initial setup

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const path = require('path')
+const PORT = process.env.PORT || 5000
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.get('/', (req, res) => res.render('pages/index'));
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
+app.get('/en', (req, res) => res.render('pages/english'));
+app.get('/de', (req, res) => res.render('pages/german'));
